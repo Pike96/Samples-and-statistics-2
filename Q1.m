a1d06c2a28e129aeab20d13888977fcfe972cf04
Yucheng Lin <Pike96@users.noreply.github.com>
2017-02-01 12:58:55 -0800

    Small edit

--- Q1.m
+++ Q1.m
@@ -11,17 +11,16 @@ for i = 1:N
 end
 disp(['The probability of rejecting the lot is ', num2str((N-S)/N)]);
 
-j = 1;
-while N-S <= ceil(0.95*N)
-    S=0;
-    j = j+1;
+test = 0;
+while N-S <= ceil(0.95*N)   % Until reach 95% rejecting
+    S=0;	% clear success times
+    test = test+1;	% Add 
     for i = 1:N
-        B = randperm(125,j);        % Test 5 microchips
-        if isempty(intersect(A,B))  % if none of 5 is member of 6 defective
+        B = randperm(125,test);     % Test 5 microchips
+        if isempty(intersect(A,B))	% if none of 5 is member of 6 defective
             S=S+1;
         end
     end
 end
-disp(['The fewest number is ', num2str(j)]);
-
+disp(['The fewest number is ', num2str(test)]);
 
