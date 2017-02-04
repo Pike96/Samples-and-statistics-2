f084a4d4051cc36dc7e3c525f56bcc3fb53bf7f0
Yucheng Lin <Pike96@users.noreply.github.com>
2017-02-04 02:40:14 -0800

    Finish Q2

--- Q2.m
+++ Q2.m
@@ -1,7 +1,7 @@
 clear;
-
+% ===== Bernoulli Trail =====
 E = 10000;          % Experiment times
-Results = zeros(1,E);
+Results1 = zeros(1,E);
 for i = 1:E
     lambda = 120;           % Expected value is 120
     interval = 0.2;         % in seconds
@@ -9,12 +9,47 @@ for i = 1:E
     p = lambda/N;           % Bernoulli prob.
     u = rand(N,1);          % Random number from 0 to 1
     bernoulliTrials = u<p;  % If smaller than prob. Success
-    Results(i) = sum(bernoulliTrials);  % Poisson(120)
+    Results1(i) = sum(bernoulliTrials);  % Poisson(120)
 end
 
 subplot(2,1,1)
 yyaxis left             
-hist(Results,20)    % Set the number of bins to 20 in histogram
+hist(Results1,20)    % Set the number of bins to 20 in histogram
+title('Number of arrivals per hour')
+xlabel('Number of arrivals')
+ylabel('Times')
+
+Range = 200;        % Plot the curve within 120 arrivals
+Theo = zeros(1,Range);
+for i = 1:Range
+    p = poisspdf(i-1,120);	% i-1 to get the prob. at 0 (trival)
+    Theo(i) = p;
+end
+
+yyaxis right
+plot(Theo,'r','linewidth',2)    % Bold, Red
+ylabel('Theorectical p.m.f.')
+set(gca,'YColor','r')           % Y Axis red
+
+% ===== Inverse Transfrom =====
+Results2 = zeros(1,E);
+for i = 1:E
+    lambda = 120;           % Expected value is 120
+    u = rand();             % Random number from 0 to 1
+    k = 0;                  % P(X=k)
+    p = exp(-lambda);       % when k=0
+    f = p;
+    while u>=f
+        p = lambda*p/(k+1);
+        f = f+p;
+        k = k+1;
+    end
+    Results2(i) = k;
+end
+
+subplot(2,1,2)
+yyaxis left
+hist(Results2,20)    % Set the number of bins to 20 in histogram
 title('Number of arrivals per hour')
 xlabel('Number of arrivals')
 ylabel('Times')
