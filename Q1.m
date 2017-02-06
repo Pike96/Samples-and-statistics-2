c10290427d2c7d65072ceaad77a583af8ac71745
Yucheng Lin <Pike96@users.noreply.github.com>
2017-02-05 21:05:49 -0800

    Small edit

--- Q1.m
+++ Q1.m
@@ -1,11 +1,11 @@
 clear;
 
-N = 1000;              % N Experiment times
+N = 10000;              % N Experiment times
 S = 0;                  % Success times
-A = randperm(125,6);    % Generate 6 Defective units without replacement
+A = randperm(125,6);	% Generate 6 Defective units without replacement
 for i = 1:N
     B = randperm(125,5);        % Test 5 microchips
-    if isempty(intersect(A,B))  % if none of 5 is member of 6 defective
+    if isempty(intersect(A,B))  % if none of 5 is 1of 6 defective
         S=S+1;
     end
 end
@@ -16,8 +16,8 @@ while N-S <= ceil(0.95*N)   % Until reach 95% rejecting
     S=0;	% clear success times
     test = test+1;	% Add 
     for i = 1:N
-        B = randperm(125,test);     % Test 5 microchips
-        if isempty(intersect(A,B))	% if none of 5 is member of 6 defective
+        B = randperm(125,test);     % Test microchips w/ increasing num
+        if isempty(intersect(A,B))	% if none of 5 is 1 of 6 defective
             S=S+1;
         end
     end
